3349b7093c0e4ee9ce6684462f5b2b8e2cf7feb1
lucifer-Hell <luciferhell58@gmail.com>
2022-07-18 18:13:50 +0530

    optimized approach

--- src/Contests/Leetcode/Weekly/Contest_301/MovePieces.java
+++ src/Contests/Leetcode/Weekly/Contest_301/MovePieces.java
@@ -1,58 +1,28 @@
 package Contests.Leetcode.Weekly.Contest_301;
 
-import java.util.Arrays;
-
 public class MovePieces {
     public static boolean canChange(String start, String target) {
-        int i=0,j=0,n=start.length(),m=target.length(),k=0;
-        while (j<m){
-            char c=target.charAt(j);
-            if(c!='_'){
-                if(c=='L'){
-                    // search for l in right
-                    boolean found=false;
-                    while( k<m && !found){
-                        if(start.charAt(k)=='R')
-                            return false;
-                        else if( start.charAt(k)=='L')
-                            found=true;
-                        else k++;
-                    }
-                    if(found){
-                        start=swap(j,k,start);
-                    }
-                    else return false;
-                }
-                else{
-                    // search for r in left
-                    boolean found=false;
-                    while( k>0 && !found){
-                        if(start.charAt(k)=='L')
-                            return false;
-                        else if( start.charAt(k)=='R')
-                            found=true;
-                        else k--;
-                    }
-                    if(found){
-                        start=swap(j,k,start);
-                    }
-                    else return false;
-                }
+        int si=0,ti=0,n=start.length();
+        while(si<n || ti<n){
+            while (si<n && start.charAt(si)=='_')si++;
+            while (ti<n && target.charAt(ti)=='_')ti++;
+            if(si>=n || ti>=n){
+                // if anyone idx reaches to end that means all chars have been found
+                // if other doesnot reaches to end that means it have some extra characters which are not valid
+                return (si==n && ti==n);
             }
-            j++;
+            char sc=start.charAt(si);
+            char tc=target.charAt(ti);
+            if(sc!=tc)return false;
+            if ((tc=='L' && si<ti) || (tc=='R' && si>ti))return false;
+            si++;ti++;
         }
-        return (start==target);
+        return true;
     }
 
-    private static String swap(int j, int k,String start) {
-        char []arr=start.toCharArray();
-        char temp=arr[k];
-        arr[k]=arr[j];
-        arr[j]=temp;
-        return new String(arr);
-    }
+
 
     public static void main(String[] args) {
-       System.out.println( canChange("_L__R__R_","L______RR"));
+       System.out.println( canChange("_L","LL"));
     }
 }
