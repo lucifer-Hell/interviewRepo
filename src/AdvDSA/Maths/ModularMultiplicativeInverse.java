f716179831ec9489ee3043e184ff4083c3b7889f
lucifer-Hell <luciferhell58@gmail.com>
2022-07-21 12:47:51 +0530

    brute approach

---
+++ src/AdvDSA/Maths/ModularMultiplicativeInverse.java
@@ -0,0 +1,16 @@
+package AdvDSA.Maths;
+
+public class ModularMultiplicativeInverse {
+    private int gcd(int a,int b){
+        return b==0?a:gcd(b,a%b);
+    }
+    public int modInverse(int a, int m)
+    {
+        if(gcd(a,m)!=1)return -1;
+        int j=1;
+        while(true){
+            if((a*j)%m==1)return j;
+            else j++;
+        }
+    }
+}
