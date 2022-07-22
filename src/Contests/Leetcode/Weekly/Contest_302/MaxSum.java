65fe308ae83956b8255b92be5db6ba2935ab5a62
lucifer-Hell <luciferhell58@gmail.com>
2022-07-22 12:55:52 +0530

    optimized approach

--- src/Contests/Leetcode/Weekly/Contest_302/MaxSum.java
+++ src/Contests/Leetcode/Weekly/Contest_302/MaxSum.java
@@ -6,48 +6,18 @@ import java.util.List;
 
 public class MaxSum {
     public int maximumSum(int[] nums) {
-        HashMap<Integer, ArrayList<Integer>>digitSumMap=new HashMap<>();
-        for(int ele : nums){
-            int digitSum=getDigitSum(ele);
-            if(digitSumMap.containsKey(digitSum)){
-                ArrayList<Integer> eleList=digitSumMap.get(digitSum);
-                if(eleList.size()==2){
-                   int minIdx=getMinIdx(eleList);
-                   if(ele>eleList.get(minIdx)){
-                       eleList.remove(minIdx);
-                       eleList.add(ele);
-                   }
-                }
-                else {
-                    // when only one element exists
-                    eleList.add(ele);
-                }
-            }
-            else{
-                ArrayList<Integer>eleList=new ArrayList<>();
-                eleList.add(ele);
-                digitSumMap.put(digitSum,eleList);
-            }
-        }
+        // 10^9 max number having 9 digits each with 9 value so 9*9=81
+        int digSumFreq[]=new int[82];
         int maxSum=-1;
-        for(int key: digitSumMap.keySet()){
-            List<Integer>eleList=digitSumMap.get(key);
-            if(eleList.size()==2)
-                maxSum=Math.max(maxSum,getSumOfAllElements(eleList));
+        for(int ele : nums) {
+            int digitSum = getDigitSum(ele);
+            if(digSumFreq[digitSum]!=0){
+                maxSum=Math.max(maxSum,digSumFreq[digitSum]+ele);
+            }
+            digSumFreq[digitSum]=Math.max(ele,digSumFreq[digitSum]);
         }
         return maxSum;
     }
-
-    private int getMinIdx(ArrayList<Integer> eleList) {
-        return eleList.get(0)>eleList.get(1)?1:0;
-    }
-
-    private int getSumOfAllElements(List<Integer> eleList) {
-        int sum=0;
-        for(int ele:eleList)sum+=ele;
-        return sum;
-    }
-
     private int getDigitSum(int ele) {
         int sum=0;
         while (ele>0){
