757eabea0869fde80cd238c1679614ac9434877c
lucifer-Hell <luciferhell58@gmail.com>
2022-07-25 17:57:41 +0530

    2343. Query Kth Smallest Trimmed Number -brute apprach

--- src/Contests/Leetcode/Weekly/Contest_302/QueryTrim.java
+++ src/Contests/Leetcode/Weekly/Contest_302/QueryTrim.java
@@ -5,87 +5,58 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class QueryTrim {
-    public static int[] smallestTrimmedNumbers(String[] nums, int[][] queries){
-        int []arr=new int [queries.length];
-        for(int i=0;i<queries.length;i++){
-                int k=queries[i][0]-1;
-                int trimLen=queries[i][1];
-                String []trimmedArray=getTrimmedArray(nums,trimLen);
-                System.out.println("trimmed array "+Arrays.toString(trimmedArray));
-                HashMap<String, ArrayList<Integer>>idxMap=getIdxMap(trimmedArray);
-                radixSort(trimmedArray);
-                int []sortedIdxArr=getSortedIdxArr(trimmedArray,idxMap);
-                arr[i]=sortedIdxArr[k];
-            }
-        return arr;
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        int ans[]=new int [queries.length];
+        int idx=0;
+        for (int[] query : queries) {
+            int k = query[0], trimLen = query[1];
+            String[] trimmedArray = getTrimmedArray(nums, trimLen);
+            int []sortedIdxArr=getSortedIdxArr(trimmedArray);
+            ans[idx]=sortedIdxArr[k-1];
+            idx++;
+        }
+        return ans;
     }
-
-    private static int[] getSortedIdxArr(String[] trimmedArray, HashMap<String, ArrayList<Integer>> idxMap) {
+    // apply radix sort
+    private int[] getSortedIdxArr(String[] trimmedArray) {
+        int baseLength=trimmedArray[0].length();
+        // call counting sort
         int []idxArr=new int [trimmedArray.length];
-        int idx=0,currIdx=0;
-        while(idx<trimmedArray.length){
-            String ele=trimmedArray[idx];
-            ArrayList<Integer>list=idxMap.get(ele);
-            if(list.size()>0){
-                idxArr[currIdx]=list.remove(0);
-                currIdx++;
-            }else idx++;
+        for(int i=0;i< trimmedArray.length;i++)
+            idxArr[i]=i;
+        for(int i=0;i<baseLength;i++)
+        {
+            idxArr=sort(trimmedArray,idxArr,i);
         }
         return idxArr;
     }
-
-    private static HashMap<String, ArrayList<Integer>> getIdxMap(String[] trimmedArray) {
-        HashMap<String, ArrayList<Integer>>idxMap=new HashMap<>();
-        for(int j=0;j<trimmedArray.length;j++){
-            String ele=trimmedArray[j];
-            if(idxMap.containsKey(ele))
-                idxMap.get(ele).add(j);
-            else{
-                ArrayList<Integer>idxList=new ArrayList<>();
-                idxList.add(j);
-                idxMap.put(ele,idxList);
-            }
+    
+    private int[] sort(String[] trimmedArray,int []currIdxArr, int baseLength) {
+        int []out =new int[trimmedArray.length];
+        int []base=new int [10];
+        // max a number in decimal system can be 9
+        for(int i=0;i< trimmedArray.length;i++){
+            int currBase=getCurrBase(trimmedArray[currIdxArr[i]],baseLength);
+            base[currBase]++;
         }
-        return idxMap;
-    }
-
-    private static void radixSort(String []arr){
-        int maxBase=arr[0].length();
-        for(int i=maxBase-1;i>=0;i--){
-            sortWithBase(arr,i);
+        // calc prefix sum
+        for(int i=1;i<=9;i++)base[i]+=base[i-1];
+        // generate out arr
+        for(int i=trimmedArray.length-1;i>=0;i--){
+            int currBase=getCurrBase(trimmedArray[currIdxArr[i]],baseLength);
+            out[base[currBase]-1]=currIdxArr[i];
+            base[currBase]--;
         }
+        return out;
     }
-
-    private static void sortWithBase(String[] arr, int base) {
-        for(int i=0;i<arr.length;i++)
-            for(int j=i+1;j<arr.length;j++)
-            {
-                int a=arr[i].charAt(base)-'0';
-                int b=arr[j].charAt(base)-'0';
-                if(a>b){
-                    swap(arr,i,j);
-                }
-            }
+    private static  int getCurrBase(String str,int baseLength){
+        return str.charAt(str.length()-1-baseLength)-'0';
     }
-    private static void swap(String[] arr, int i, int j) {
-        String temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-
-    private static String[] getTrimmedArray(String[] nums, int trimLen) {
-        String []arr=new String [nums.length];
-        for(int i=0;i<nums.length;i++){
-            arr[i]=getTrimmedString(nums[i],trimLen);
-        }
-        return arr;
-    }
-
-    private static String getTrimmedString(String num, int trimLen) {
-        return num.substring(num.length()-trimLen);
-    }
-
-    public static void main(String[] args) {
-        smallestTrimmedNumbers(new String []{"64333639502","65953866768","17845691654","87148775908","58954177897","70439926174","48059986638","47548857440","18418180516","06364956881","01866627626","36824890579","14672385151","71207752868"},new int[][] {{9,4},{6,1},{3,8},{12,9},{11,4},{4,9},{2,7},{10,3}, {13,1},{13,1},{6,1},{5,10}});
+    private String[] getTrimmedArray(String[] nums, int trimLen) {
+        String[] out = new String[nums.length];
+        for(int i=0;i<nums.length;i++)
+            out[i]=nums[i].substring(nums[i].length()-trimLen);
+        return out;
     }
 }
+
